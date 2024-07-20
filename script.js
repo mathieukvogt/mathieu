9769b716893df0491363b44c30bf83dba1bf95f6
Mathieu <mathieukvogt@yahoo.com>
2024-07-21 01:21:10 +0200

    index fix

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+import smoothscroll from "smoothscroll-polyfill";
+
+// kick off the polyfill!
+smoothscroll.polyfill();
+
 document.querySelectorAll(".alphsort a").forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -11,5 +16,14 @@ document.querySelectorAll(".alphsort a").forEach((anchor) => {
         behavior: "smooth",
       });
     }
+  });
+});
+
+document.querySelectorAll(".alphsort a").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    targetElement.scrollIntoView({ behavior: "smooth" });
   });
 });
