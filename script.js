cad3aa179b708e600007ec30c3ba5577b94ad80d
Mathieu <mathieukvogt@yahoo.com>
2024-07-22 01:28:55 +0200

    scroll fix

--- script.js
+++ script.js
@@ -3,27 +3,15 @@ import smoothscroll from "smoothscroll-polyfill";
 // kick off the polyfill!
 smoothscroll.polyfill();
 
-document.querySelectorAll(".alphsort a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    const targetId = this.getAttribute("href").substring(1);
-    const targetElement = document.getElementById(targetId);
-
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: "smooth",
-      });
-    }
-  });
-});
-
-document.querySelectorAll(".alphsort a").forEach((link) => {
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-    const targetId = this.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({ behavior: "smooth" });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".alphsort a").forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    });
   });
 });
