7e4b75de8805b8acbc33cdb77841e51c39e02a2a
Mathieu <mathieukvogt@yahoo.com>
2024-09-04 18:55:45 +0200

    button

--- script.js
+++ script.js
@@ -16,20 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-import smoothscroll from "smoothscroll-polyfill";
-
-// kick off the polyfill!
-smoothscroll.polyfill();
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".alphsort a").forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      const targetId = this.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: "smooth" });
-      }
-    });
+document.getElementById("upButton").onclick = function () {
+  document.getElementById("variables").scrollIntoView({
+    behavior: "smooth",
   });
-});
+};
