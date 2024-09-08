1954f59e78899a46896f39f1b0fb0f5c438e6ca4
Mathieu <mathieukvogt@yahoo.com>
2024-09-08 23:22:26 +0200

    content

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 import smoothscroll from "smoothscroll-polyfill";
 
-// Kick off the polyfill for smooth scroll
+// kick off the polyfill!
 smoothscroll.polyfill();
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Ensure the DOM is fully loaded before adding event listeners
-
-  // Smooth scroll for links with class "alphsort"
+  // Existing smooth scroll functionality
   document.querySelectorAll(".alphsort a").forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
@@ -18,15 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Toggle dark mode when dark mode button is clicked
+  // Dark mode toggle functionality
   const darkModeButton = document.getElementById("darkModeButton");
-  if (darkModeButton) {
-    darkModeButton.addEventListener("click", function () {
-      document.body.classList.toggle("dark-mode");
-    });
-  } else {
-    console.error(
-      'Dark mode button not found. Ensure the button exists with id="darkModeButton".'
-    );
-  }
+  darkModeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
 });
