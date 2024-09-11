8ca882b9fde0ef0065b855e4562d9125c8888558
Mathieu <mathieukvogt@yahoo.com>
2024-09-12 00:26:17 +0200

    loader

--- script.js
+++ script.js
@@ -11,31 +11,43 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Dark mode toggle functionality
   const darkModeButton = document.getElementById("darkModeButton");
 
+  // Check localStorage for the preferred mode
+  const currentMode = localStorage.getItem("mode");
+
+  // If a mode is found, apply it
+  if (currentMode === "dark") {
+    document.documentElement.classList.add("dark-mode");
+    document.body.classList.add("dark-mode");
+    darkModeButton.textContent = "LM"; // Light Mode
+  } else {
+    darkModeButton.textContent = "DM"; // Dark Mode
+  }
+
+  // Toggle dark mode and store the preference
   darkModeButton.addEventListener("click", function () {
-    // Toggle dark-mode class on both html and body
     document.documentElement.classList.toggle("dark-mode");
     document.body.classList.toggle("dark-mode");
 
-    // Check current mode and update button text
     if (document.body.classList.contains("dark-mode")) {
       darkModeButton.textContent = "LM"; // Light Mode
+      localStorage.setItem("mode", "dark"); // Save the preference to localStorage
     } else {
       darkModeButton.textContent = "DM"; // Dark Mode
+      localStorage.setItem("mode", "light"); // Save the preference to localStorage
     }
   });
-});
 
-gsap.to(".bar", 1.5, {
-  delay: 0,
-  height: 0,
-  stagger: {
-    amount: 0.5,
-  },
-  ease: "power4.inOut",
-  onComplete: function () {
-    document.querySelector(".overlay").style.display = "none";
-  },
+  gsap.to(".bar", 1.3, {
+    delay: 0,
+    height: 0,
+    stagger: {
+      amount: 0.5,
+    },
+    ease: "power4.inOut",
+    onComplete: function () {
+      document.querySelector(".overlay").style.display = "none";
+    },
+  });
 });
