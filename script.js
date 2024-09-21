be0fb823a5be6da193f9e82b2bcf190eeaf372a9
Mathieu <mathieukvogt@yahoo.com>
2024-09-21 23:15:44 +0200

    menu update

--- script.js
+++ script.js
@@ -97,10 +97,3 @@ const burgerButton = document.querySelector(".burger");
 burgerButton.addEventListener("click", () => {
   burgerButton.classList.toggle("rotate");
 });
-
-const menuButton = document.querySelector(".burger");
-const body = document.querySelector("body");
-
-menuButton.addEventListener("click", () => {
-  body.classList.toggle("no-scroll");
-});
